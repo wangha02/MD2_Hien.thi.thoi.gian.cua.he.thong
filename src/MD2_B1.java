c4be3c49941a92df3981e39d44fc8cdfd9a0a3d7
unknown <btkkentxx2002@gmail.com>
2022-07-24 22:37:18 +0700

    jv

---
+++ src/MD2_B1.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class MD2_B1 {
+    public static void main(String[] args) {
+        Date date = new Date(); // lấy ra thời gian hiện tại
+        System.out.println("Now is:" +date); //In ra thời gian hiện tại
+    }
+}
